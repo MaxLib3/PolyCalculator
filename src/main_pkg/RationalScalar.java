9a63a43eaba5012ef24f55a54d2ce17adfc48d21
Etai <yaronet@post.bgu.ac.il>
2025-04-11 19:54:32 +0300

    commiting after finishing class RationalScalar

--- src/main_pkg/RationalScalar.java
+++ src/main_pkg/RationalScalar.java
@@ -11,18 +11,22 @@ public class RationalScalar implements Scalar
     }
 
     @Override
-    public Scalar add(Scalar s) {
-        return null;
+    public Scalar add(Scalar s){
+        s.addRational(this);
     }
 
     @Override
     public Scalar addInteger(IntegerScalar s) {
-        return null;
+
+        return s.addRational(this);
     }
 
     @Override
     public Scalar addRational(RationalScalar s) {
-        return null;
+
+        int numerator1 = ((s.getNumerator() * denominator) + (numerator * s.getDenominator()));
+        int denominator1 = (s.getDenominator() * denominator);
+        return new RationalScalar(numerator1, denominator1);
     }
 
     @Override
@@ -32,27 +36,40 @@ public class RationalScalar implements Scalar
 
     @Override
     public Scalar mulInteger(IntegerScalar s) {
-        return null;
+        return s.mulRational(this);
     }
 
     @Override
     public Scalar mulRational(RationalScalar s) {
-        return null;
+        int numerator1 = (s.getNumerator() * numerator);
+        int denominator1 = (s.getDenominator() * denominator);
+        return new RationalScalar(numerator1, denominator1);
     }
 
     @Override
     public Scalar neg() {
-        return null;
+        return this.mulInteger(new IntegerScalar(-1));
     }
 
     @Override
     public Scalar power(int exponent) {
-        return null;
+        if (this.sign() < 0)
+            throw new IllegalArgumentException();
+        Scalar s = new RationalScalar(1, 1);
+        for (int i = 0; i < exponent; i++){
+            s = s.mul(this);
+        }
+        return s;
     }
 
     @Override
     public int sign() {
-        return 0;
+        if (numerator == 0)
+            return 0;
+        else if ( (numerator < 0 && denominator > 0) || (numerator > 0 && denominator < 0) )
+            return -1;
+        else
+            return 1;
     }
 
     public int getNumerator() {
